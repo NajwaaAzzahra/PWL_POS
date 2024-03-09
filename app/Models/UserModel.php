2a8a780c5f7d20da6924078aa4b2b97e1d180637
Najwa Azzahra <najwaazzahra0507@gmail.com>
2024-03-09 15:32:05 +0700

    perubahan js 4 2.1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,7 +10,7 @@ class UserModel extends Model
     use HasFactory;
 
     protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primarykey = 'user_id'; //mendefinisikan primary key yang digunakan
+    protected $primaryKey = 'user_id'; //mendefinisikan primary key yang digunakan
 
     //js 4 percobaan 1
     protected $fillable=['level_id', 'username', 'nama'];
